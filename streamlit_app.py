70f8d6da266031e6c91979d65e99693bc4b8362e
Pfm2024 <166004231+Pfm2024@users.noreply.github.com>
2024-04-18 19:23:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,13 @@
 import streamlit as st
 
-edad=10
-
 url40="https://www.youtube.com/watch?v=HKStqRFOEWg"
 url20="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+edad=st.slider("edad", min_value=10, max_value=70, step=2)
+st.write(f"Tu edad es ...{edad}")
 
 if edad > 40:
   st.video(url40)
 else:
   st.video(url20)
-
-edad=st.slider("edad", min_value=10, max_value=70, step=2)
-st.write(f"Tu edad es ...{edad}")
 
